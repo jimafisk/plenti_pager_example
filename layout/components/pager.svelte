b6a812d442d0ba59d93c898d6d95dcce3a3b62f8
jimafisk <jimafisk@gmail.com>
2020-12-31 15:10:15 -0500

    Style pager.

--- layout/components/pager.svelte
+++ layout/components/pager.svelte
@@ -5,7 +5,10 @@
 <ul>
   {#if currentPage > 1}
     <li><a href="/">first</a></li>
-    <li><a href="/{currentPage - 1}">previous</a></li>
+    <li><a href="/{currentPage - 1}">prev</a></li>
+  {:else}
+    <li><span>first</span></li>
+    <li><span>prev</span></li>
   {/if}
   {#each [3,2,1] as i}
     {#if currentPage - i > 0}
@@ -21,12 +24,17 @@
   {#if currentPage < totalPages}
     <li><a href="/{currentPage + 1}">next</a></li>
     <li><a href="/{totalPages}">last</a></li>
+  {:else}
+    <li><span>next</span></li>
+    <li><span>last</span></li>
   {/if}
 </ul>
 
 <style>
   ul {
     display: flex;
+    max-width: 600px;
+    margin: 20px auto;
   }
   li {
     flex-grow: 1;
@@ -37,5 +45,10 @@
     padding: 5px 20px;
     border-radius: 5px;
     background: #f0efef;
+  }
+  span {
+    text-align: center;
+    padding: 5px 20px;
+    color: gray;
   }
 </style>
